056dec6e482ea4b56bf16d1986ec374bd9051421
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-11-20 19:42:17 +0630

    Add Volunteer Flash Message before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :persist_search
+  before_filter :flash_user
   
   protected
     def persist_search
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
 
     def authorize_super_admin
       redirect_to :back, alert: "You can't perform this action" unless current_user.present? && current_user.super_admin?
+    end
+
+    def flash_user
+      flash[:volunteer] = "Thanks for volunteering with us. You will be able to help us after we confirmed you as moderator. Stay tuned!"
     end
 end
